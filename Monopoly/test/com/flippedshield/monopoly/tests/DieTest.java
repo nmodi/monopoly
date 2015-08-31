4b7f24e18756292c9e8ec96dea38cd74032569ab
Jacob <jacob.pagano@gmail.com>
2015-08-31 16:13:17 -0500

    added another Die class test

--- Monopoly/test/com/flippedshield/monopoly/tests/DieTest.java
+++ Monopoly/test/com/flippedshield/monopoly/tests/DieTest.java
@@ -2,8 +2,7 @@ package com.flippedshield.monopoly.tests;
 
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.flippedshield.monopoly.Die;
@@ -12,21 +11,13 @@ public class DieTest {
 	
 	private Die die;
 
-	@Before
-	public void setUp() throws Exception {
-		die = new Die();
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	/**
 	 * test no param roll returns populated array
 	 */
 	public void testRollNoParams() 
 	{
+		die = new Die(6);
 		assertNotNull(die.roll());
 	}
 	
@@ -36,7 +27,33 @@ public class DieTest {
 	 */
 	public void testRollWithParams() 
 	{
-		assertNotNull(die.roll(2));
+		die = new Die(6);
+		assertNotNull(die.roll(4));
+	}
+	
+	@Test
+	public void testRollReturnsValuesBetweenOneAndSix() throws Exception
+	{
+		int sides = 6;
+		int expectedMax = 6;
+		int[] roll;
+		
+		die = new Die(sides);
+		
+		roll = die.roll(2);
+		
+		
+		
+		for(int i = 0; i < roll.length; i++)
+		{
+			if(roll[i] < 0)
+			{
+				throw new Exception("Dice " + i + " roll is less than 0");
+			} else if(roll[i] > expectedMax)
+			{
+				throw new Exception("Dice " + i + " roll is greater than max");
+			}
+		}
 	}
 
 }
