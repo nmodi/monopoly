999fe731bc0d7f8733cc14153f8cec65cd2696f4
Jacob <jacob.pagano@gmail.com>
2015-09-01 08:45:15 -0500

    exception merge

--- Monopoly/test/com/flippedshield/monopoly/tests/DieTest.java
+++ Monopoly/test/com/flippedshield/monopoly/tests/DieTest.java
@@ -1,7 +1,11 @@
 package com.flippedshield.monopoly.tests;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 import com.flippedshield.monopoly.Die;
