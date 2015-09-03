b4a82a769c7b655fd8e76407faef72da0665f6e7
Jacob <jacob.pagano@gmail.com>
2015-09-03 09:43:00 -0500

    added deed test

--- Monopoly/src/com/flippedshield/monopoly/Player.java
+++ Monopoly/src/com/flippedshield/monopoly/Player.java
@@ -2,4 +2,20 @@ package com.flippedshield.monopoly;
 
 public class Player {
 
+	private String name;
+	
+	/**
+	 * Creates player with name
+	 * @param name
+	 */
+	public Player(String name)
+	{
+		setName(name);
+	}
+	
+	public String getName() { return name; }
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
